dbbc408a88a0720bd50f3bb67c853787e6a53222
Chris <106836962+Cujion@users.noreply.github.com>
2023-02-06 16:35:51 -0500

    added tsparticles background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Particle from './components/Particle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <Particle/>
+
+    <div>
+      <h1>Hello This is my React Portfolio</h1>
     </div>
+    </>
   );
 }
 
---
+++ src/components/Particle.jsx
@@ -0,0 +1,99 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+function Particle() {
+
+    const ParticlesInit = async (main) => {
+        console.log(main);
+
+
+        await loadFull(main);
+    };
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+  return (
+    <Particles
+    id="tsparticles"
+        init={ParticlesInit}
+        loaded={particlesLoaded}
+
+        options={{
+            background: {
+                color: {
+                    value: "#9b8b74",
+                },
+            },
+            fpsLimit: 120,
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "push",
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                    resize: true,
+                },
+                modes: {
+                    push: {
+                        quantity: 4,
+                    },
+                    repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                    },
+                },
+            },
+            particles: {
+                color: {
+                    value: "#000000",
+                },
+                links: {
+                    color: "#ffffff",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                },
+                collisions: {
+                    enable: true,
+                },
+                move: {
+                    directions: "none",
+                    enable: true,
+                    outModes: {
+                        default: "bounce",
+                    },
+                    random: false,
+                    speed: 6,
+                    straight: false,
+                },
+                number: {
+                    density: {
+                        enable: true,
+                        area: 800,
+                    },
+                    value: 80,
+                },
+                opacity: {
+                    value: 0.5,
+                },
+                shape: {
+                    type: "circle",
+                },
+                size: {
+                    value: { min: 1, max: 5 },
+                },
+            },
+            detectRetina: true,
+        }}
+    />
+  );
+}
+
+export default Particle
