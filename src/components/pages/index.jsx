6cbec8790707f521cf7382123188cad81f561b50
Chris <106836962+Cujion@users.noreply.github.com>
2023-02-08 15:15:30 -0500

    added comments to index

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -1,8 +1,10 @@
+// IMPORTING ALL PAGES 
 import About from "./About";
 import Portfolio from "./Portfolio";
 import Contact from "./Contact";
 import Resume from "./Resume";
 
+// ASSIGNING PAGES AND OBJECT THAT TAKES IN EACH PAGE
 const pages = { About, Portfolio, Contact, Resume };
 
 export default pages;
